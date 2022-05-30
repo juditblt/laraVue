012b7c8a9139eda36c6ad2a40b8f55f112fb30e3
juditblt <nagymiklosi.judit@blathy.info>
2022-05-30 13:58:09 +0200

    Initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <br>
+    <hr>
+    Hello Mindenki!
+    <hr>
+    <CounterButton :startv="10" :min="1" :max="13"></CounterButton>
+    <br>
+    <!-- Így is lehet írni: -->
+    <counter-button :startv="2"></counter-button>
+    <br>
+    <CounterButton :startv="0"></CounterButton>
+    <hr>
+    <br>
+    <TemplateBox></TemplateBox>
+</template>
+
+<script>
+import CounterButton from "./components/CounterButton";
+import TemplateBox from "./components/TemplateBox";
+export default {
+    name: "App",
+    components: {
+        CounterButton,
+        TemplateBox
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
